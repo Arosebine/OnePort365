c1c7e3f645bf26e60797675f9c74f5413ceaac62
Arowolo Ebine <arowolo.ebine@lampnets.com>
2024-02-07 08:19:32 +0100

    remove error.log files

--- taskProcessor/database/taskWorker.js
+++ taskProcessor/database/taskWorker.js
@@ -1,8 +1,4 @@
 const mongoose = require("mongoose")
-// const cluster = require("cluster")
-// const CPU_CORE = 2;
-
-// const { receiveMessage } = require("../services/fetch_task");
 
 const connectDB = async () => {
     try {
@@ -15,14 +11,5 @@ const connectDB = async () => {
 }
 
 
-// if(cluster.isMaster){
-//     console.log(`Master is up PID: ${process.pid}`)
-//     for(var i=0;i<CPU_CORE;i++){
-//         cluster.fork();
-//     }
-// }else{
-//     console.log(`Worker is up PID: ${process.pid}`)
-//     receiveMessage();
-// }
 
 module.exports = connectDB;
--- taskProcessor/middlewares/rateLimit.js
+++
@@ -1,9 +0,0 @@
-const rateLimit = require('express-rate-limit');
-
-const limiter = rateLimit({
-  windowMs: 60 * 60 * 1000, // 1 hour
-  max: 5,
-  message: 'Too many requests from this IP, please try again after an hour.'
-});
-
-module.exports = limiter;
